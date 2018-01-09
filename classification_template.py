fd8cd1e3c0264c6fbf6027e6e5e4c06c2c5aa861
drblur <ashwinkumar.chalkapure@gmail.com>
2018-01-09 17:02:00 +0530

    few minor changes

--- classification_template.py
+++ classification_template.py
@@ -4,9 +4,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
+
 
 # Importing the dataset
+
 dataset = pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, 4].values
